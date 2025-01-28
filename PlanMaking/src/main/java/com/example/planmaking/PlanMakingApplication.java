00f290604f104a9194a8fb8749d1c79abddff1d8
ity0526 <ity0526@gmail.com>
2025-01-28 18:00:43 +0900

    [Feat] 초기 세팅

---
+++ PlanMaking/src/main/java/com/example/planmaking/PlanMakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.planmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanMakingApplication.class, args);
+    }
+
+}
---
+++ PlanMaking/src/test/java/com/example/planmaking/PlanMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanMakingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
